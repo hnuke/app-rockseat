741d842446a8382528373f106b576b68a1b92f50
higor <hstuquifatec@gmail.com>
2024-09-10 12:13:42 -0300

    fundamentos00

---
+++ index.js
@@ -0,0 +1,9 @@
+// hello world
+
+let msg = "Hello, world!" // a variavel de tipo let permite o dado da variavel trocar de tipo
+msg = 1023;
+
+const msg1 = 10; // o tipo de variavel Const não permite a variavel o dado trocar de tipoter seu valor trocado
+
+console.log(msg);
+
