2ac7034565cbbd462c65d47fff9dad29f36fed78
higor <hstuquifatec@gmail.com>
2024-09-11 18:12:12 -0300

    Listagem de metas

--- index.js
+++ index.js
@@ -1,13 +1,73 @@
-const start = () => {
+const { select, input, checkbox } = require('@inquirer/prompts')
+
+let metas = []
+
+const cadastrarMeta = async () => {
+    const meta = await input({message: "Digite a meta: "})
+
+    if(meta.length == 0) {
+        console.log("A meta não pode ser vazia.")
+        return
+    }
+
+    metas.push({value: meta, checked: false})
+    
+}
+
+
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message:"Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
+        choices: [...metas],
+        instructions: false,
+    })
+
+    if (respostas.length == 0){
+        console.log("Nenhuma meta selecionada!")
+        return
+    }
+
+    metas.forEach((n) =>{
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+    console.log("Metas concluídas ou não...")
+
+}
+
+const start = async () => {
 
     while(true){
-        let opcao = "cadastrar"
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {name: "Cadastrar meta",
+                value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {name: "Sair",
+                value: "sair"
+                }
+            ]
+        })
         switch(opcao){
             case "cadastrar":
-                console.log("vamos cadastrar")
+                await cadastrarMeta()
+                console.log(metas)
                 break
             case "listar":
-                console.log("vamos listar")
+                await listarMetas()
                 break
             case "sair":
                 return
