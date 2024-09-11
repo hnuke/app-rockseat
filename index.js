7a518e9e25e8c89153cd2bf30b96eab07f254fdd
higor <hstuquifatec@gmail.com>
2024-09-11 17:32:08 -0300

    fundamentos002

--- index.js
+++ index.js
@@ -1,38 +1,9 @@
-// hello world
+function start() {
+    while(true){
 
-let msg = "Hello, world!" // a variavel de tipo let permite o dado da variavel trocar de tipo
-msg = 1023;
-
-const msg1 = 10; // o tipo de variavel Const não permite a variavel o dado trocar de tipoter seu valor trocado
-
-console.log(msg);
-
-
-// arrays, objetos
-
-let metas = ["aluno", "prof"];
-
-console.log(metas[0]);
-
-// objetos
-
-let meta = {
-    value:"Ler um livro por mês",
-    checked: false,
-    isChecked: () => {}, // método
-    print: () => {
-        console.log(this);
+        return
     }
-}
-
-console.log(meta.value + " , checado: " + meta.checked);
+} // Função para iniciar aplicação
 
 
-// function  
-                    // array function
-const criarMeta = () => {}
-
-
-function criarMeta(){
-
-}
+start();
